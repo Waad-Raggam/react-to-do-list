85bc958cd3c8bc52466ec657cede1df79ed7cb05
Waad-Raggam <108654364+Waad-Raggam@users.noreply.github.com>
2024-07-06 18:33:37 +0300

    Setup template

---
+++ src/List.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const List = () => {
+  return (
+    <div className="card1 card text-white bg-dark mb-3 border-dark">
+      <div class="card-header">
+        To Do List
+      </div>
+      <div className="card-body">
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Add item"></input>
+        {/* <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </p> */}
+        <button type="submit" class="btn btn-primary">Add task</button>
+      </div>
+      {/* <div>
+      <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></input>
+        <label class="form-check-label" for="defaultCheck1">
+          Default checkbox
+        </label>
+      </div> */}
+    </div>
+  );
+};
+
+export default List;
