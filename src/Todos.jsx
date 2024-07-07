3989688aa00c3b15832a57ea9efe82449bbca081
Waad-Raggam <108654364+Waad-Raggam@users.noreply.github.com>
2024-07-07 14:02:51 +0300

    Make a list of todos

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -1,13 +1,27 @@
 const Todos =()=>{
+    const TodoItem = [
+        {
+            id: 1,
+            name: "Task 1"
+        }, {
+            id: 2,
+            name: "Task 2"
+        }, {
+            id: 3,
+            name: "Task 3"
+        }
+    ];
     return(
-        <table class="table table-dark">
+        <div>{
+            TodoItem.map((todo)=>(
+<table class="table table-dark">
   <tbody>
     <tr>
-      <td>Mark</td>
+      <td>{todo.name}</td>
       <td><i class="bi bi-pencil"></i></td>
       <td><i class="bi bi-trash"></i></td>
     </tr>
-    <tr>
+    {/* <tr>
       <td>Jacob</td>
       <td><i class="bi bi-pencil"></i></td>
       <td><i class="bi bi-trash"></i></td>
@@ -16,9 +30,13 @@ const Todos =()=>{
       <td>Larry</td>
       <td><i class="bi bi-pencil"></i></td>
       <td><i class="bi bi-trash"></i></td>
-    </tr>
+    </tr> */}
   </tbody>
 </table>
+            ))
+            }
+             
+        </div>
     );
 };
 
